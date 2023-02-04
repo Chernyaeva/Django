3633156b0fb5239d6bd95de97db344941b26db5c
anastasia <moiri@mail.ru>
2023-02-04 19:33:22 +0100

    Lesson 9 Locust test

--- config/celery.py
+++ config/celery.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 if settings.DEBUG:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
